14eb70055662c0502dc679b3fa778e64671f903f
jay <jayvish2000>
2022-07-22 17:08:53 +0530

    first commit

--- routers/products.js
+++ routers/products.js
@@ -22,7 +22,7 @@ const storage = multer.diskStorage({
         cb(uploadError, 'public/uploads')
     },
     filename: function (req, file, cb) {
-        const fileName = file.originalname.split('').join('-');
+        const fileName = file.originalname.split(' ').join('-');
         const extension = FILE_TYPE_MAP[file.mimetype];
         cb(null, `${fileName}-${Date.now()}.${extension}`)
     }
